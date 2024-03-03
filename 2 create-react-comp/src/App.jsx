7bf52a00df40393a978eac6459d86bc9b31ab480
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-03-03 08:50:43 +0530

    Still Learning

---
+++ 2 create-react-comp/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./Hello.jsx"
+
+function App() {
+  return <div>
+    <h1>
+      Hello World
+    </h1>
+    <Hello />
+  </div>
+}
+
+export default App
---
+++ 2 create-react-comp/src/Hello.jsx
@@ -0,0 +1,14 @@
+function Hello() {
+
+    let myName = 'Tarun';
+    let fullName = () => {
+        return 'Tarun Joshi'
+    }
+
+    return <h3>
+        Hello this is the future speaking
+        I am your master {fullName()}
+    </h3>
+}
+
+export default Hello
