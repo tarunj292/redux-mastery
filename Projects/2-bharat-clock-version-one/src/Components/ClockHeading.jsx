cd0f338df70346eb4bba7bca2ff7c3bd69968d42
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-03-05 11:36:02 +0530

    clock version one ready

---
+++ Projects/2-bharat-clock-version-one/src/Components/ClockHeading.jsx
@@ -0,0 +1,9 @@
+function ClockHeading() {
+    return (
+        <>
+            <h1 className="fw-bolder" >Bharat clock</h1>
+        </>
+    )
+}
+
+export default ClockHeading;
---
+++ Projects/2-bharat-clock-version-one/src/Components/ClockSlogan.jsx
@@ -0,0 +1,9 @@
+function ClockSlogan() {
+    return (
+        <>
+            <p className="lead">This is a clock that shows the time in Bharat at all times</p>
+        </>
+    )
+}
+
+export default ClockSlogan;
