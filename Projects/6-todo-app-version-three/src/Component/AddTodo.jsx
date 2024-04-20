0d6ea8de2b1e189f17949bc039728e91f45df12f
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-04-20 11:56:25 +0530

    working on third version implementing useState

---
+++ Projects/6-todo-app-version-three/src/Component/AddTodo.jsx
@@ -0,0 +1,17 @@
+function AddTodo() {
+    return (
+        <div className="row">
+            <div className="col-4">
+                <input type="text" placeholder="Enter Todo Here" />
+            </div>
+            <div className="col-4">
+                <input type="date" />
+            </div>
+            <div className="col-2">
+                <button type="button" className="btn btn-success kg-button">Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default AddTodo
