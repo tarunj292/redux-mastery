2291e82fb7a06cf3edc66fa6bfab5780facf1ab8
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-07-09 13:37:17 +0530

    working

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -3,13 +3,20 @@ import './App.css'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Sidebar from "./components/Sidebar"
+import CreatePost from "./components/CreatePost"
+import PostList from "./components/PostList"
 
 function App() {
+
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
     <div className="app-container">
       <Sidebar />
       <div className="content">
         <Header />
+        <CreatePost />
+        <PostList />
         <Footer />
       </div>
     </div>
--- Projects/7-social-media/src/components/Sidebar.jsx
+++ Projects/7-social-media/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ const Sidebar = () => {
     return (
         <>
             <div>
-                <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{ width: "280px" }}>
+                <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{ width: "280px" }}>
                     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <svg class="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
                         <span class="fs-4">Sidebar</span>
@@ -18,25 +18,7 @@ const Sidebar = () => {
                         <li>
                             <a href="#" class="nav-link text-white">
                                 <svg class="bi pe-none me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link text-white">
-                                <svg class="bi pe-none me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
-                                Orders
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link text-white">
-                                <svg class="bi pe-none me-2" width="16" height="16"><use xlinkHref="#grid"></use></svg>
-                                Products
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link text-white">
-                                <svg class="bi pe-none me-2" width="16" height="16"><use xlinkHref="#people-circle"></use></svg>
-                                Customers
+                                Create Post
                             </a>
                         </li>
                     </ul>
