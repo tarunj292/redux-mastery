2466789dd2d2d8bef1080202b2e6f19288941fce
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-09-03 21:45:52 +0530

    work in progress

---
+++ Redux/Counter React Redux/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
+import DisplayCounter from './components/DisplayCounter'
+
+function App() {
+
+  return (
+    <>
+      <div className="px-4 py-5 my-5 text-center">
+
+        <Header />
+        <div className="col-lg-6 mx-auto">
+          <DisplayCounter />
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Redux/Counter React Redux/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <h1 className="display-5 fw-bold text-body-emphasis">
+            Counter
+        </h1>
+    )
+}
+
+export default Header;
