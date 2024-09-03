f2f3ef1eca66c34541e3b7a9c382351339a60bfe
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-09-03 22:05:02 +0530

    Improving UI/UX with bootstrap

--- Redux/Counter React Redux/src/App.jsx
+++ Redux/Counter React Redux/src/App.jsx
@@ -2,21 +2,21 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import DisplayCounter from './components/DisplayCounter'
+import Container from './components/Container'
+import Controls from './components/Controls'
 
 function App() {
 
   return (
     <>
       <div className="px-4 py-5 my-5 text-center">
-
-        <Header />
-        <div className="col-lg-6 mx-auto">
-          <DisplayCounter />
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        <Container>
+          <Header />
+          <div className="col-lg-6 mx-auto">
+            <DisplayCounter />
+            <Controls />
           </div>
-        </div>
+        </Container>
       </div>
     </>
   )
---
+++ Redux/Counter React Redux/src/components/Controls.jsx
@@ -0,0 +1,12 @@
+const Controls = () => {
+    return (
+        <>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+            </div>
+        </>
+    )
+}
+
+export default Controls;
