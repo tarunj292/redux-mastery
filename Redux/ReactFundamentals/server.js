44c52de20d9e5106bd8ad483fe406c72f9dba534
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-09-03 19:44:32 +0530

    a demo to practice ready

--- Redux/ReactFundamentals/server.js
+++ Redux/ReactFundamentals/server.js
@@ -1,3 +1,24 @@
 const redux = require('redux');
 
-redux.createStore()
+const INITIAL_VALUE = {
+    counter: 0
+};
+
+const reducer = (store = INITIAL_VALUE, action) => {
+    let newStore = store;
+    if (action.type == 'INCREMENT') {
+        newStore.counter += 1;
+    }
+    return newStore;
+};
+
+const store = redux.createStore(reducer);
+
+const subscriber = () => {
+    const state = store.getState();
+    console.log(state);
+}
+
+store.subscribe(subscriber)
+
+store.dispatch({ type: 'INCREMENT' });
