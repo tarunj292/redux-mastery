1361a032b35566e3812e634f152161d06785e94f
Tarun Joshi <156983323+tarunj292@users.noreply.github.com>
2024-09-03 19:17:05 +0530

    Delete 2 create-react-comp directory

--- 2 create-react-comp/src/Random.jsx
+++
@@ -1,8 +0,0 @@
-function Random() {
-
-    let number = Math.random() * 10;
-
-    return <h1 style={{ 'background-color': 'red' }}>Random Number is: {Math.round(number)}</h1>
-}
-
-export default Random;
